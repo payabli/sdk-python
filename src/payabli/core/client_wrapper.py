6b945a5b4114a0ad3fae8d5384d6e932c1b0572d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-15 15:08:52 +0000

    Release 0.0.288

--- src/payabli/core/client_wrapper.py
+++ src/payabli/core/client_wrapper.py
@@ -22,10 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "payabli/0.0.287",
+            "User-Agent": "payabli/0.0.288",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "payabli",
-            "X-Fern-SDK-Version": "0.0.287",
+            "X-Fern-SDK-Version": "0.0.288",
             **(self.get_custom_headers() or {}),
         }
         headers["requestToken"] = self.api_key
